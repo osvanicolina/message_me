a697566891e841a3b3d4436d26a66f7666d0a158
Osvani <osvacolijim@gmail.com>
2024-01-08 10:50:45 -0400

    Setup semantic ui

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # Defines the route for the "login" page ("/login")
+  get  'login', to: 'sessions#new'
 end
