c3cb2761bf59aeccca0132a13878c5aedf57cc46
Osvani <osvacolijim@gmail.com>
2024-01-10 11:36:24 -0400

    Start authentication system

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::Base
+    helper_method :current_user, :logged_in?
+
+    def current_user
+        @current_user ||= User.find(session[:user_id]) if session[:user_id] # If current_user is already set, don't hit the database again
+    end
+
+    def logged_in?
+        !!current_user # !! turns current_user into a boolean
+    end
+
+    def require_user
+        if !logged_in?
+            flash[:alert] = "You must be logged in to perform that action"
+            redirect_to login_path
+        end
+    end
 end
